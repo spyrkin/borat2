896753353b5fefc346360cb562397a76deeae553
sergey <sergey.pyrkin@instatsport.com>
2018-03-25 17:28:59 +0300

    shw

---
+++ KamikyIt/KamikyForms/Gui/AdviceControl.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace KamikyForms.Gui
+{
+    /// <summary>
+    /// Логика взаимодействия для AdviceControl.xaml
+    /// </summary>
+    public partial class AdviceControl : Canvas
+    {
+        public AdviceControl()
+        {
+            InitializeComponent();
+            setResourcec();
+        }
+
+        private void setResourcec()
+        {
+            double margin = (Width - mainBlock.Width) / 2;
+            Canvas.SetLeft(mainBlock, margin);
+            String t = Name;
+            if (t == "hobby")
+            {
+                adviceName.Content = "УВЛЕЧЕНИЯ";
+            }
+        }
+
+
+        //нажатие на кнопку
+
+        private void onChose(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
