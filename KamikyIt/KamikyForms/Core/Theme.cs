cbc96edc05e6aae208bcaa9400d0797a2f3442e3
sergey <sergey.pyrkin@instatsport.com>
2018-09-07 12:16:29 +0300

    Theme start

---
+++ KamikyIt/KamikyForms/Core/Theme.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KamikyForms.Core
+{
+
+    //Тема беседы
+    public class Theme
+    {
+        public string Name;
+        public List<ThemeItem> messages = new List<ThemeItem>();
+        public int curIndex;
+
+        public class ThemeItem
+        {
+            public int n;
+            public string message;
+        }
+    }
+}
